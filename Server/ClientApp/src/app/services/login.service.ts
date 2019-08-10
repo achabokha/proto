39e681580378098f60edb16521c640b11aa9a78f
Andrei Chabokha <achabokha@hotmail.com>
2019-08-10 00:37:09 -0400

    minor crap

--- Server/ClientApp/src/app/services/login.service.ts
+++ Server/ClientApp/src/app/services/login.service.ts
@@ -3,31 +3,31 @@ import { Observable, of, Observer, throwError } from 'rxjs';
 import { EventEmitter } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class LoginService {
 
-  userId: number = null;
-  username = '';
-  name = '';
-  email = '';
-  isLoggedIn: Observable<boolean> = of(false);
-  onLoggedInOut: Observer<any> = {
-    next: x => {
-      this.userId = x ? 1 : null;
-      this.isLoggedIn = of(x);
-    },
-    error: err => console.error('Observer got an error: ' + err),
-    complete: () => console.log('Observer got a complete notification'),
-  };
+    userId: number = null;
+    username = '';
+    name = '';
+    email = '';
+    isLoggedIn: Observable<boolean> = of(false);
+    onLoggedInOut: Observer<any> = {
+        next: x => {
+            this.userId = x ? 1 : null;
+            this.isLoggedIn = of(x);
+        },
+        error: err => console.error('Observer got an error: ' + err),
+        complete: () => console.log('Observer got a complete notification'),
+    };
 
-  constructor() { }
+    constructor() { }
 
-  login(userName: string, userPasw: string): Observable<any> {
-    if (userName && userPasw) {
-      const responseObs = of({ userName, email: `${userName}@taiga.com` });
-      return responseObs;
+    login(username: string, password: string): Observable<any> {
+        if (username && password) {
+            const responseObs = of({ username, email: `${username}@taiga.com` });
+            return responseObs;
+        }
+        throwError('Something went terribly wrong!');
     }
-    throwError('Something wen terrible wrong');
-  }
 }
