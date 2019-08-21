4c0496350e38830edb199e10c63e6afbc9d6fca2
Andrejs Saboha <andrejssaboha@gmail.com>
2019-08-21 20:26:54 +0300

    Search box

--- Server/ClientApp/src/app/search/search.module.ts
+++ Server/ClientApp/src/app/search/search.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CustomMaterialModuleModule } from '../custom-material-module/custom-material-module.module';
+
 
 
 @NgModule({
   declarations: [SearchComponent],
   imports: [
     CommonModule,
-    SearchRoutingModule
+    CustomMaterialModuleModule,
+    SearchRoutingModule,
+    FlexLayoutModule
   ]
 })
 export class SearchModule { }
